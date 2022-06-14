0bc5dd54cf2e728b49b737b59a396d6ce79c3ee1
vanpetr <vanpetr@yandex.ru>
2022-06-15 00:22:23 +0300

    commit jupiter book

--- progect 0/game_v2.py
+++ progect 0/game_v2.py
@@ -5,7 +5,7 @@
 import numpy as np
 
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+    """Угадываем число используя дихотомию с предварительным разделением диапазона
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -16,12 +16,26 @@ def random_predict(number:int=1) -> int:
         
     count = 0
     min_number = 0 
+    medium_number1 = 25 # задаем отрезки для ускорения поиска
+    medium_number2 = 50
+    medium_number3 = 75
     max_number = 100
+    if number <= medium_number1:
+            max_number =  medium_number1
+    elif number <= medium_number2:
+            max_number = medium_number2
+            min_number = medium_number1
+    elif number <= medium_number3:
+            max_number = medium_number3
+            min_number = medium_number2 
+    else:
+         max_number = 100
+         min_number = medium_number3  
 
     while True:
         count += 1
         predit_number = round((min_number+max_number)/2) # предполагаемое число
-        
+            
         if predit_number > number:
             max_number = predit_number
         elif predit_number < number:
